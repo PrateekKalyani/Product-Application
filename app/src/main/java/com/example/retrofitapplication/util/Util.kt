e08bf6764439becdac54b4e2413b609a21f9892e
PrateekKalyani <93260333+PrateekKalyani@users.noreply.github.com>
2021-12-13 03:18:53 +0530

    app updated

--- app/src/main/java/com/example/retrofitapplication/util/Util.kt
+++ app/src/main/java/com/example/retrofitapplication/util/Util.kt
@@ -6,7 +6,8 @@ import android.net.NetworkCapabilities
 import android.os.Build
 import javax.inject.Inject
 
-class Util @Inject
+class Util
+@Inject
 constructor(
     private val context: Context
 ){
@@ -14,29 +15,21 @@ constructor(
     fun checkForInternet(): Boolean {
 
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
             val network = connectivityManager.activeNetwork ?: return false
-
             val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
 
             return when {
-
                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-
                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-
                 else -> false
             }
         } else {
-
             @Suppress("DEPRECATION") val networkInfo =
                 connectivityManager.activeNetworkInfo ?: return false
             @Suppress("DEPRECATION")
             return networkInfo.isConnected
         }
-
         return false
     }
 }
